31c8503ceb18e62ce5bbd4fd6705018ec6b17e52
Jair Marques <jairsmarques1126@gmail.com>
2025-04-16 18:46:34 -0300

    Resolvendo o exercício de fixação da aula sobre Construtores e encapsulamento

---
+++ 08.1-exercicio-de-fixaxao/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account account;
+
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		System.out.print("Is there an initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+		if (response == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			double initiaDeposit = sc.nextDouble();
+			account = new Account(number, name, initiaDeposit);
+		} else {
+			account = new Account(number, name);
+		}
+
+		System.out.println();
+		System.out.println("Account data:");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depoitValue = sc.nextDouble();
+		account.deposit(depoitValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+
+		sc.close();
+
+	}
+
+}
